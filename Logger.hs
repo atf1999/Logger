56eb682b065d8229d44f457fc32b9d2093e33279
atf1999 <torth212@gmail.com>
2015-07-14 16:53:08 -0400

    Basic File logging finished

--- Logger.hs
+++ Logger.hs
@@ -1,16 +1,29 @@
 module Logger where
 import Data.Time
 
+--Intializes the file
 init f =  writeFile f ""
 
 currentTime = fmap show getCurrentTime
                
-
+--Debug: Message used purely for debugging
 --f = file name 
---m = message to be concated to the file
+--m = message to be concatenated to the file
 debug f m = do
           time <- currentTime
           let msg = time ++ " DEBUG: " ++ m ++ "\n"
           appendFile f msg
-          
-       
+--Alert: Message used to show the programmer that an error or important event occurred
+--f = file name
+--m = message to be concatenated to the file 
+alert f m = do
+	  time <- currentTime
+          let msg = time ++ " ALERT: " ++ m ++ "\n"
+          appendFile f msg
+--Deadly: If a scenario could occur that could cause the program to crash, use this method to tell you when it happens
+--f = file name
+--m = message to be concatenated to the file
+deadly f m = do
+            time <- currentTime
+            let msg = time ++ " DEADLY: " ++ m ++ "\n"
+            appendFile f msg
--- Main.hs
+++ Main.hs
@@ -2,4 +2,7 @@ module Main where
 import Logger
 main = do
        Logger.init "test.txt"
-       Logger.debug "test.txt" "Debug"
+       Logger.debug "test.txt" "Debug info"
+       Logger.alert "test.txt" "Something alerting happened"
+       Logger.deadly "test.txt" "Something deadly, possibly program breaking happened"
+       
